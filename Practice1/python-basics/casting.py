cebf299abea79d64b1efd53af29fa16ddeb33b5c
Ayaulym <aaulymkhantarbaj@gmail.com>
2026-01-26 21:41:54 +0500

    Practice1 in progress

---
+++ Practice1/python-basics/casting.py
@@ -0,0 +1,24 @@
+#1
+a = "2.8"
+b =float(a)
+print(b)
+
+#2
+c = 4
+d = 3
+result = int(c / d)
+print(result)
+
+#4
+age = 17
+text = str(age)
+print(text)
+
+#5
+x = input("Enter a number: ")
+y = int(x)
+print(y + 5)
+
+#6
+z=int(2.8) 
+print(z)
