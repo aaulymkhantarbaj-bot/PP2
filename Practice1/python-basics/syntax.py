7c76e2582066960f7c5216dc8b2c721ed980ae63
Ayaulym <aaulymkhantarbaj@gmail.com>
2026-01-26 20:59:20 +0500

    Practice1 in progress

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,20 @@
+#1
+x=18
+if x>=18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!")
+
+#3
+x=20
+name = "Ayaulym"
+pi=3.14
+print(x)
+print(name)
+print(pi)
