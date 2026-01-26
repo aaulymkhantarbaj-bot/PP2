07679784ca43261889e2fe41f680aa4eaeee9323
Ayaulym <aaulymkhantarbaj@gmail.com>
2026-01-26 21:31:24 +0500

    Practice1 in progress

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,20 @@
+#1
+a=10 #integer
+
+#2 
+b=3.14 #float
+
+#3
+l = ["apple", "banana", "cherry"]  #list
+
+#4
+y=False #bool
+
+
+#5
+s="Aruzhan" #str
+print(type(a))
+print(type(b))
+print(type(l))
+print(type(y)) 
+print(type(s))         
