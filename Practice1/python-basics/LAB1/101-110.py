b62543e0a18248e69235980b266c6454b28f8cdb
Ayau <aaulymkhantarbaj@gmail.com>
2026-01-27 17:20:15 +0500

    lab1

---
+++ Practice1/python-basics/LAB1/101-110.py
@@ -0,0 +1,50 @@
+#1
+name=input()
+print("Hello, "+name+"!")
+
+#2
+a=input()
+b=input()
+print(a+"***"+b)
+
+#3
+s=input()
+if s.isdigit:
+    print("int")
+else:
+    print("str")
+
+#4
+a=int(input())
+b=int(input())
+print(a+b)
+
+#5
+a=int(input())
+b=int(input())
+print(a//b)
+print(a/b)
+
+#6
+a=int(input())
+b=int(input())
+print(pow(a,b))
+
+#7
+a=int(input())
+b=int(input())
+print(a%b)
+
+#8
+s=input()
+n=int(input())
+print(s*n)
+
+#9
+s=input()
+print(len(s))
+
+#10
+s=input()
+print(s.upper())
+print(s.lower())
