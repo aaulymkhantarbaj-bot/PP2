82e71087cdbf7e8d18d8474d16bb41ec428e3495
Ayau <aaulymkhantarbaj@gmail.com>
2026-02-02 19:59:26 +0500

    lab2

--- Practice2/LAB2/LAB2/201-210.py
+++ Practice2/LAB2/LAB2/201-210.py
@@ -5,3 +5,100 @@ if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
     print("YES")
 else:
     print("NO")
+
+#2
+n = int(input())
+total = 0
+for i in range(1, n + 1):
+    total += i
+print(total)
+
+#3
+n = int(input())
+a = list(map(int, input().split()))
+
+s = 0
+for i in range(n):
+    s = s + a[i]
+
+print(s)
+
+#4
+n = int(input())
+a = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    if a[i] > 0:
+        cnt += 1
+
+print(cnt)
+
+#5
+n = int(input())
+
+if n <= 0:
+    print("NO")
+else:
+    while n % 2 == 0:
+        n = n // 2
+
+    if n == 1:
+        print("YES")
+    else:
+        print("NO")
+
+#6
+n = int(input())
+a = input().split()
+
+mx = int(a[0])
+for i in range(1, n):
+    if int(a[i]) > mx:
+        mx = int(a[i])
+
+print(mx)
+
+#7
+n = int(input())
+a = list(map(int, input().split()))
+
+max_value = a[0]
+max_pos = 1 
+
+for i in range(1, n):
+    if a[i] > max_value:
+        max_value = a[i]
+        max_pos = i + 1  
+
+print(max_pos)
+
+#8
+n = int(input())
+
+power = 1
+while power <= n:
+    print(power, end=" ")
+    power *= 2
+
+#9
+n = int(input())
+a = list(map(int, input().split()))
+
+mx = max(a)
+mn = min(a)
+
+for i in range(n):
+    if a[i] == mx:
+        a[i] = mn
+
+for x in a:
+    print(x, end=" ")
+
+#10
+n = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True)
+print(*a)
+
+
---
+++ Practice2/LAB2/LAB2/211-220.py
@@ -0,0 +1,141 @@
+#11
+n, l, r = map(int, input().split())
+a = list(map(int, input().split()))
+
+l -= 1
+r -= 1
+
+while l < r:
+    a[l], a[r] = a[r], a[l]
+    l += 1
+    r -= 1
+
+for x in a:
+    print(x, end=" ")
+
+#12
+n = int(input())
+a = list(map(int, input().split()))
+
+for i in range(n):
+    a[i] = a[i] * a[i]
+
+for x in a:
+    print(x, end=" ")
+
+#13
+n = int(input())
+
+if n <= 1:
+    print("No")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("No")
+            break
+    else:
+        print("Yes")
+
+#14
+n = int(input())
+a = list(map(int, input().split()))
+
+freq = {}
+for x in a:
+    freq[x] = freq.get(x, 0) + 1
+
+mx = max(freq.values())
+ans = min(x for x in freq if freq[x] == mx)
+
+print(ans)
+
+#15
+n = int(input())
+surnames = set()
+
+for _ in range(n):
+    surnames.add(input())
+
+print(len(surnames))
+
+#16
+n = int(input())
+a = list(map(int, input().split()))
+
+seen = set()
+for x in a:
+    if x in seen:
+        print("NO")
+    else:
+        print("YES")
+        seen.add(x)
+
+#17
+n = int(input())
+freq = {}
+
+for _ in range(n):
+    num = input()
+    freq[num] = freq.get(num, 0) + 1
+
+count = 0
+for v in freq.values():
+    if v == 3:
+        count += 1
+
+print(count)
+
+#18
+n = int(input())
+arr = []
+
+for i in range(1, n + 1):
+    s = input()
+    arr.append((s, i))
+
+first_pos = {}
+for s, i in arr:
+    if s not in first_pos:
+        first_pos[s] = i
+
+for s in sorted(first_pos):
+    print(s, first_pos[s])
+
+#19
+n = int(input())
+d = {}
+
+for _ in range(n):
+    name, k = input().split()
+    k = int(k)
+    d[name] = d.get(name, 0) + k
+
+for name in sorted(d):
+    print(name, d[name])
+
+#20
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+db = {}
+out = []
+
+for _ in range(n):
+    line = input().rstrip('\n')
+
+    if line.startswith("set"):
+        parts = line.split(maxsplit=2)
+        key = parts[1]
+        value = parts[2] if len(parts) == 3 else ""
+        db[key] = value
+
+    else:  
+        key = line.split()[1]
+        if key in db:
+            out.append(db[key])
+        else:
+            out.append(f"KE: no key {key} found in the document")
+
+sys.stdout.write("\n".join(out))
